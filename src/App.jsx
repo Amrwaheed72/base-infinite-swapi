b514953dc1f3d84b5ec1ed6a3acabdd61336b7fa
Amr Waheed <amrmohamed877@gmail.com>
2025-04-23 20:33:26 +0200

    ..

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./App.css";
 import { InfinitePeople } from "./people/InfinitePeople";
 import { InfiniteSpecies } from "./species/InfiniteSpecies";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 function App() {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 0,
+      },
+    },
+  });
   return (
-    <div className="App">
-      <h1>Infinite SWAPI</h1>
-      <InfinitePeople />
-      {/* <InfiniteSpecies /> */}
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools />
+      <div className="App">
+        <h1>Infinite SWAPI</h1>
+        <InfinitePeople />
+        {/* <InfiniteSpecies /> */}
+      </div>
+    </QueryClientProvider>
   );
 }
 
--- src/people/InfinitePeople.jsx
+++ src/people/InfinitePeople.jsx
@@ -1,5 +1,6 @@
 import InfiniteScroll from "react-infinite-scroller";
 import { Person } from "./Person";
+import { useInfiniteQuery } from "@tanstack/react-query";
 
 const initialUrl = "https://swapi.dev/api/people/";
 const fetchUrl = async (url) => {
@@ -9,5 +10,13 @@ const fetchUrl = async (url) => {
 
 export function InfinitePeople() {
   // TODO: get data for InfiniteScroll via React Query
+  const { data, fetchNextPage, hasNextPage } = useInfiniteQuery({
+    queryKey: ["sw-people"],
+    queryFn: ({ pageParam = initialUrl }) => fetchUrl(pageParam),
+    getNextPageParam:(lastPage)=>{
+      return lastPage.next;
+    }
+  });
+  console.log(data)
   return <InfiniteScroll />;
 }
